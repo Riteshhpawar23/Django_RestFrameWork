39823302a40aa2239bb7237f6f4ff97bc68043a5
Ritesh Git <riteshpawar0223@gmail.com>
2025-09-11 01:46:52 +0530

    student app with table is created

--- Api/views.py
+++ Api/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
 
 # Create your views here.
+def studentview(request):
+    return JsonResponse({'name':'Ravi','age':24})
