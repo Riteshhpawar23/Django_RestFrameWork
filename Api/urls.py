98bd68d76e34f10d9ded725e23b94d7ea9c9a4f7
Ritesh Pawar <riteshpawar2223@ternaengg.ac.in>
2025-09-14 23:33:20 +0530

    removed teacher blogs section

--- Api/urls.py
+++ Api/urls.py
@@ -3,7 +3,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Test endpoint
-    path('hello/', views.hello_world, name='hello-world'),
-    # Add your API endpoints here
 ]
